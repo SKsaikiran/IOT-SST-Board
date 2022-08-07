c1582146e0490ad5724afe6ec945307a28e5edaf
Sai kiran J <saikiranj2002@gmail.com>
2022-08-07 22:08:24 +0530

    Blink the RGB led

---
+++ BLINK_RGB_LED/BLINK_RGB_LED.ino
@@ -0,0 +1,46 @@
+
+void setup() 
+{
+  pinMode(0, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(16, OUTPUT);
+  digitalWrite(4, LOW);
+
+}
+
+void loop() 
+{
+  digitalWrite(0, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(16, LOW);
+  delay(1000);
+  digitalWrite(0, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(16, HIGH);
+  delay(1000);
+  digitalWrite(0, LOW);
+  digitalWrite(5, HIGH);
+  digitalWrite(16, LOW);
+  delay(1000);
+  digitalWrite(0, LOW);
+  digitalWrite(5, HIGH);
+  digitalWrite(16, HIGH);
+  delay(1000);
+  digitalWrite(0, HIGH);
+  digitalWrite(5, LOW);
+  digitalWrite(16, LOW);
+  delay(1000);
+  digitalWrite(0, HIGH);
+  digitalWrite(5, LOW);
+  digitalWrite(16, HIGH);
+  delay(1000);
+  digitalWrite(0, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(16, LOW);
+  delay(1000);
+  digitalWrite(0, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(16, HIGH);
+  delay(1000);
+}
