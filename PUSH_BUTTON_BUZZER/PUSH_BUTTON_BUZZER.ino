3c05e8bfc495c61b73a24035e3c292432bc07445
Sai kiran J <saikiranj2002@gmail.com>
2022-08-08 21:18:36 +0530

    controlling buzzer using push button

---
+++ PUSH_BUTTON_BUZZER/PUSH_BUTTON_BUZZER.ino
@@ -0,0 +1,25 @@
+const int toggle=16; //GPIO16
+int buttonstate = 0;
+const int buzzer=12;
+void setup() 
+{
+  // put your setup code here, to run once:
+  pinMode(2,OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  buttonstate=digitalRead(toggle);
+  Serial.print(buttonstate);
+  if(buttonstate == 1)
+   {
+    digitalWrite(buzzer,HIGH);
+   }
+   else if(buttonstate == 0)
+   {
+   digitalWrite(buzzer,LOW);
+   }
+   
+}
