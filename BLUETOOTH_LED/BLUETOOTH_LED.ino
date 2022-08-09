2ec167173c4d805e6eaf6fb88a1d806e52e78438
Sai kiran J <saikiranj2002@gmail.com>
2022-08-09 21:01:09 +0530

    Interfacing Bluetooth module & controlling LED .

---
+++ BLUETOOTH_LED/BLUETOOTH_LED.ino
@@ -0,0 +1,22 @@
+
+ #include <SoftwareSerial.h>
+ SoftwareSerial mySerial(14,12); // RX(D5),TX(D6)
+ void setup() 
+ {
+  // Open serial communications and wait for port to open:
+     Serial.begin(9600);
+     mySerial.begin(9600);
+    pinMode(2,OUTPUT);
+  }
+ void loop() 
+ { // run over and over
+  char  val;
+   if (mySerial.available()) 
+    {
+        val=mySerial.read();
+        if(val=='1')
+        digitalWrite(2,HIGH);
+        if(val=='0')
+        digitalWrite(2,LOW);
+        }
+ }
