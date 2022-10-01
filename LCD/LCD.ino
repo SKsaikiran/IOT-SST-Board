853bf2c1134856c71949b4746b15e1a3f3d8265e
Arjun Hegde <arjunmeega28@gmail.com>
2022-10-01 07:09:03 +0530

    Update LCD.ino

--- LCD/LCD.ino
+++ LCD/LCD.ino
@@ -20,13 +20,13 @@ void loop(){
     lcd.setCursor(0, 0);
     // print message
     lcd.print("Hello World!");
-    delay(1000);
+    delay(2000);
     // clears the display to print new message
     lcd.clear();
     // set cursor to first column, second row
     lcd.setCursor(0,1);
     lcd.print("Hello World!");
-    delay(1000);
+    delay(2000);
     lcd.clear(); 
 }
 
