f3c95d8a2edcd200d5436a915a8993a7347f1ac6
Sai kiran J <saikiranj2002@gmail.com>
2022-10-18 18:59:03 +0530

    .

--- OLED/OLED.ino
+++ OLED/OLED.ino
@@ -12,7 +12,6 @@ void loop(void) {
   u8g2.clearBuffer // clear the internal memory
   u8g2.setFont(u8g2_font_ncenB08_tr);	// choose a suitable font
   u8g2.drawStr(50,35,"  ");	// write something to the internal memory to print within double quotes as ("HELLO WORLD")
-  u8g2.sendBuffer();					// transfer internal memory to the display
+  u8g2.sendBuffer();// transfer internal memory to the display
   delay(1000);  
 }
-
