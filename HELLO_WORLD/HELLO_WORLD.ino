f3a5e7f34d302aa8edd286233525f5ca657cad3b
Sai kiran J <saikiranj2002@gmail.com>
2022-11-01 23:35:06 +0530

    To print hello world and var value

--- HELLO_WORLD/HELLO_WORLD.ino
+++ HELLO_WORLD/HELLO_WORLD.ino
@@ -1,13 +1,14 @@
 int a=10;
+float b=20.99;
 void setup() 
 {
   // put your setup code here, to run once:
  Serial.begin(9600);
 }
-
 void loop() 
 {
   // put your main code here, to run repeatedly:
   Serial.println("Hello World");
   Serial.println(a);
+  Serial.println(b);
 }
